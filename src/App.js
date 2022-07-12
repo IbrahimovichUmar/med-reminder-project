7fa032ac1909a08465450e1ffc242aa69afa0673
Umar <umaribragimovich555@gmail.com>
2022-07-12 12:24:15 +0500

    feat: fixing mistakes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+//import { phoneBanner1, phoneBanner2, phoneBanner3 } from "./images";
 
 function App() {
-  return <div className="wrapper">Hello World</div>;
+  return (
+    <div className="med__reminder">
+      <div className="banner">
+        <div className="banner__green">
+          <div className="phone__img">Hello world</div>
+          <div className="banner__text">
+            <p>
+              Why be worried about{"\n"}
+              forgetting to take your{"\n"}
+              medicine when you have
+            </p>
+          </div>
+        </div>
+        <p>MED REMINDER</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
