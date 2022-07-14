adbe807b2201bb0c81f3bb4f464eaec52264b921
Umar <umaribragimovich555@gmail.com>
2022-07-14 10:00:31 +0500

    feat: sub-banner

--- src/App.js
+++ src/App.js
@@ -66,6 +66,24 @@ function App() {
         </div>
       </div>
       {/* <ContactUs /> */}
+      <div className="sub__banner">
+        <div className="sub__banner__content wrapper">
+          <p>
+            Medreminder is a free pill reminder and medication tracker app. You
+            may make doctor's appointments, learn more about the medications
+            you're taking and their side effects, track your health, and share
+            your health status with your family and friends. Up to three people
+            profiles can be added in an account at once, and anyone from
+            anywhere in the world can view your loved one's account by
+            invitation.
+            <br />
+            <p>
+              Join the hundreds of satisfied users who see our app as the
+              greatest of all medication reminder apps.
+            </p>
+          </p>
+        </div>
+      </div>
       <footer>
         <div className="footer__banner">
           <div className="wrapper">
