b4f9ceb5ab53fe95c6e84a140c49480232ae902e
Umar <umaribragimovich555@gmail.com>
2022-07-14 08:41:27 +0500

    feat: fixing-mistakes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 //import logo from "./logo.svg";
-import bannerImg from "./assets/images/banner/banner1.png";
+import bannerImg from "./assets/images/banner/banner.png";
 import logo from "./assets/images/header/logo.svg";
-//import ContactUs from "./contact/index";
 import appStore from "./assets/images/banner/app-store.png";
 import playStore from "./assets/images/banner/play-store.png";
+import platform1 from "./assets/images/footer/footer-platform1.jpg";
+import platform2 from "./assets/images/footer/footer-platform2.jpg";
+
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FiTwitter } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+
+// import ContactUs from "./contact/index";
 
 function App() {
   return (
@@ -26,10 +33,9 @@ function App() {
           </ul>
         </div>
       </header>
-
       <div className="banner">
         <div className="banner__content">
-          <div className="banner__inner__contentw">
+          <div className="banner__inner__content">
             <div className="first">
               <img src={bannerImg} />
             </div>
@@ -58,8 +64,56 @@ function App() {
           </a>
         </div>
       </div>
-
       {/* <ContactUs /> */}
+      <footer>
+        <div className="footer__banner">
+          <div className="wrapper">
+            <div className="footer__head">
+              <p className="company__footer">© MEDREMINDER 2021</p>
+              <ul className="footer__navbar">
+                <li>Resources</li>
+                <li>Surveys</li>
+                <li>Press Releases</li>
+                <li>Careers</li>
+              </ul>
+            </div>
+
+            <div className="footer__platform">
+              <div className="left">
+                <p>LEFT</p>
+              </div>
+              <div className="right">
+                <img src={platform1} alt="/" />
+
+                <img src={platform2} alt="/" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer__icons wrapper">
+          <a
+            href="https://www.instagram.com/"
+            className="insta"
+            target="blanck"
+          >
+            <AiOutlineInstagram font-size="35px" />
+          </a>
+          <a
+            href="https://www.twitter.com/"
+            className="twitter"
+            target="blanck"
+          >
+            <FiTwitter font-size="35px" />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            className="facebook"
+            target="blanck"
+          >
+            <FiFacebook font-size="35px" />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
